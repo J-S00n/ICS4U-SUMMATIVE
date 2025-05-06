fb91d4b50dbc935aa3d4067a6e3296a84e2380c7
J-S00n <jason.chen20@student.tdsb.on.ca>
2025-05-05 22:35:10 -0400

    Hid Google API Key

--- src/firebase/index.js
+++ src/firebase/index.js
@@ -1,16 +1,16 @@
-// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+import dotenv from "dotenv";
+
+dotenv.config();
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyAmneMT85pSP_knAZFRfwsCOpdJWUI8U-I",
-  authDomain: "ics4u-e6485.firebaseapp.com",
-  projectId: "ics4u-e6485",
-  storageBucket: "ics4u-e6485.firebasestorage.app",
-  messagingSenderId: "691115731061",
-  appId: "1:691115731061:web:8dcc5a71455ac3eeaef03c"
+  apiKey: process.env.FIREBASE_API_KEY,
+  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.FIREBASE_PROJECT_ID,
+  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.FIREBASE_APP_ID,
 };
 
 // Initialize Firebase
